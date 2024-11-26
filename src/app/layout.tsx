e438f89a33e37e1d20e8ddcd0922aae64732e2b9
Steven Frady <sfrady20@gmail.com>
2024-11-26 02:03:08 -0500

    analytics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <AppProvider>
           {children}
           <Toaster richColors />
+          <GoogleAnalytics gaId="G-NKJ667V116" />
         </AppProvider>
       </body>
     </html>
--- src/components/analytics/index.ts
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import { install } from "ga-gtag";
-
-process.env.NODE_ENV === "production" && install("G-NKJ667V116");
